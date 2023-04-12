668a8b7071302302e4261cb4cbdc32b8ed7dfacb
Andryel Blanco <andryelmblanco@gmail.com>
2023-04-12 19:18:25 -0300

    Creating DbContext & User Class

---
+++ WeeLink_API/Utils/Context/ContextDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeeLink_Domain.Entities.User;
+
+namespace WeeLink_API.Utils.Context;
+
+public class ContextDb : DbContext
+{
+   
+    public ContextDb(DbContextOptions<ContextDb> options) : base(options)
+    {
+
+    }
+
+    public DbSet<User> User { get; set; }
+}
---
+++ WeeLink_API/Utils/Extension/ContextDbExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeeLink_API.Utils.Context;
+
+namespace WeeLink_API.Utils.Extension;
+
+public static class ContextDbExtension
+{
+    public static void AddDatabaseContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        string? connectionString = configuration.GetConnectionString("ConnectionString");
+        services.AddDbContext<ContextDb>(options => options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 32))));
+    }
+}
