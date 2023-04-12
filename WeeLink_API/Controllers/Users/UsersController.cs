a020c8406383cdf4fbaa4b138aa7cf1b8d440da0
Andryel Blanco <andryelmblanco@gmail.com>
2023-04-12 20:06:06 -0300

    Creating users Controller + migrations

---
+++ WeeLink_API/Controllers/Users/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeeLink_API.Utils.Context;
+using WeeLink_Domain.Entities.User;
+
+namespace WeeLink_API.Controllers.Users
+{
+    public class UsersController : Controller
+    {
+        private readonly ContextDb _contextDb;
+
+        public UsersController(ContextDb contextDb)
+        {
+            _contextDb = contextDb;
+        }
+
+        [HttpGet]
+        public IActionResult Index([FromBody] User newUser)
+        {
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = "teste",
+                Email = "teste2",
+                Password = "123Teste",
+                CreatedAt = DateTime.Now
+            };
+
+            // Adiciona o novo usuário ao contexto do banco de dados e salva as alterações
+            _contextDb.User.Add(user);
+            _contextDb.SaveChanges();
+
+            // Retorna uma resposta HTTP indicando que o usuário foi adicionado com sucesso
+            return Ok();
+
+        }
+    }
+}
