41a4f210c45cea05baeea8bf3b746263129462eb
Andryel Blanco <andryelmblanco@gmail.com>
2023-04-12 21:43:44 -0300

    Fixing some bugs and restructuring some folders

---
+++ WeeLink_API/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeeLink_API.Utils.Context;
+using WeeLink_Domain.Entities.User;
+
+namespace WeeLink_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ContextDb _contextDb;
+
+        public UserController(ContextDb contextDb)
+        {
+            _contextDb = contextDb;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var newUser = new User()
+            {
+                Id = Guid.NewGuid(),
+                Name = "UEe",
+                Email = "uee",
+                Password = "ueee",
+                CreatedAt = DateTime.Now,
+            };
+
+            _contextDb.User.Add(newUser);
+            _contextDb.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
