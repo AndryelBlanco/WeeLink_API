f9c858dfa9f73743881f944bf9f49bfe1c0ae12c
Andryel Blanco <andryelmblanco@gmail.com>
2023-04-23 17:08:30 -0300

    Hashing Password

---
+++ WeeLink_Infrastructure/Criptography/PasswordEncrypt.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace WeeLink_Infrastructure.Criptography;
+
+public class PasswordEncrypt
+{
+    public string Encrypt(string pwd)
+    {
+        var bytes = Encoding.UTF8.GetBytes(pwd);
+        var sha512 = SHA512.Create();
+        byte[] hashBytes = sha512.ComputeHash(bytes);
+        return StringBytes(hashBytes);
+    }
+
+    private string StringBytes(byte[] hashBytes)
+    {
+        var sb = new StringBuilder();
+        foreach (byte b in hashBytes)
+        {
+            var hex = b.ToString("x2");
+            sb.Append(hex);
+        }
+
+        return sb.ToString();
+    }
+}
